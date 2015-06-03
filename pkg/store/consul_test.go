a79c0b5afcc6177625d4d37de4b3d1cb810c9367
Alexandre Beslic <abronan@docker.com>
2015-06-02 19:53:33 -0700

    Add DeleteTree tests
    Signed-off-by: Alexandre Beslic <abronan@docker.com>

--- pkg/store/consul_test.go
+++ pkg/store/consul_test.go
@@ -264,3 +264,70 @@ func TestConsulList(t *testing.T) {
 		}
 	}
 }
+
+func TestConsulDeleteTree(t *testing.T) {
+	kv := makeConsulClient(t)
+
+	prefix := "nodes/"
+
+	firstKey := "nodes/first"
+	firstValue := []byte("first")
+
+	secondKey := "nodes/second"
+	secondValue := []byte("second")
+
+	// Put the first key
+	if err := kv.Put(firstKey, firstValue, nil); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Put the second key
+	if err := kv.Put(secondKey, secondValue, nil); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Get should work on the first Key
+	pair, err := kv.Get(firstKey)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if pair.Value == nil {
+		t.Fatalf("expected value: %#v", firstValue)
+	}
+	if !bytes.Equal(pair.Value, firstValue) {
+		t.Fatalf("unexpected value: %#v", pair.Value)
+	}
+	if pair.LastIndex == 0 {
+		t.Fatalf("unexpected index: %#v", pair.LastIndex)
+	}
+
+	// Get should work on the second Key
+	pair, err = kv.Get(secondKey)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if pair.Value == nil {
+		t.Fatalf("expected value: %#v", secondValue)
+	}
+	if !bytes.Equal(pair.Value, secondValue) {
+		t.Fatalf("unexpected value: %#v", pair.Value)
+	}
+	if pair.LastIndex == 0 {
+		t.Fatalf("unexpected index: %#v", pair.LastIndex)
+	}
+
+	// Delete Values under directory `nodes`
+	if err := kv.DeleteTree(prefix); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Get should fail on both keys
+	pair, err = kv.Get(firstKey)
+	if err == nil && pair != nil {
+		t.Fatalf("err: %v", err)
+	}
+	pair, err = kv.Get(secondKey)
+	if err == nil && pair != nil {
+		t.Fatalf("err: %v", err)
+	}
+}
--- pkg/store/etcd_test.go
+++ pkg/store/etcd_test.go
@@ -253,3 +253,70 @@ func TestEtcdList(t *testing.T) {
 		}
 	}
 }
+
+func TestEtcdDeleteTree(t *testing.T) {
+	kv := makeEtcdClient(t)
+
+	prefix := "nodes/"
+
+	firstKey := "nodes/first"
+	firstValue := []byte("first")
+
+	secondKey := "nodes/second"
+	secondValue := []byte("second")
+
+	// Put the first key
+	if err := kv.Put(firstKey, firstValue, nil); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Put the second key
+	if err := kv.Put(secondKey, secondValue, nil); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Get should work on the first Key
+	pair, err := kv.Get(firstKey)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if pair.Value == nil {
+		t.Fatalf("expected value: %#v", firstValue)
+	}
+	if !bytes.Equal(pair.Value, firstValue) {
+		t.Fatalf("unexpected value: %#v", pair.Value)
+	}
+	if pair.LastIndex == 0 {
+		t.Fatalf("unexpected index: %#v", pair.LastIndex)
+	}
+
+	// Get should work on the second Key
+	pair, err = kv.Get(secondKey)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if pair.Value == nil {
+		t.Fatalf("expected value: %#v", secondValue)
+	}
+	if !bytes.Equal(pair.Value, secondValue) {
+		t.Fatalf("unexpected value: %#v", pair.Value)
+	}
+	if pair.LastIndex == 0 {
+		t.Fatalf("unexpected index: %#v", pair.LastIndex)
+	}
+
+	// Delete Values under directory `nodes`
+	if err := kv.DeleteTree(prefix); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Get should fail on both keys
+	pair, err = kv.Get(firstKey)
+	if err == nil && pair != nil {
+		t.Fatalf("err: %v", err)
+	}
+	pair, err = kv.Get(secondKey)
+	if err == nil && pair != nil {
+		t.Fatalf("err: %v", err)
+	}
+}
